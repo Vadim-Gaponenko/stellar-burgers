e4ef0340486020b377a59aa69207568fb795be34
Vadim-Gaponenko <139169525+Vadim-Gaponenko@users.noreply.github.com>
2024-08-27 15:45:15 +0300

    test

--- app-header/app-header.tsx
+++
@@ -1,9 +0,0 @@
-import { FC } from 'react';
-import { AppHeaderUI } from '@ui';
-import { useSelector } from '../../services/store';
-import { getName } from '../../services/slices/userSlice';
-
-export const AppHeader: FC = () => {
-  const userName = useSelector(getName);
-  return <AppHeaderUI userName={userName} />;
-};
--- src/utils/burger-api.ts
+++ src/utils/burger-api.ts
@@ -1,7 +1,8 @@
+const URL = 'https://norma.nomoreparties.space/api';
+
+// const URL = process.env.BURGER_API_URL;
 import { setCookie, getCookie } from './cookie';
 import { TIngredient, TOrder, TOrdersData, TUser } from './types';
-
-const URL = process.env.BURGER_API_URL;
 
 const checkResponse = <T>(res: Response): Promise<T> =>
   res.ok ? res.json() : res.json().then((err) => Promise.reject(err));
