067e8b1ce1e7eff84371e6a4660209ce866a000b
Gaponenko Vadim <mandarinsev@yandex.ru>
2024-09-26 00:35:39 +0300

    new3 25.00

--- src/services/store.test.js
+++ src/services/store.test.js
@@ -1,107 +1,62 @@
-import store from './store';
-import {
-  addItem,
-  clearAll,
-  updateAll
-} from './slices/constructorIngredientSlice';
+import { configureStore } from '@reduxjs/toolkit';
+import store, { RootState } from './store';
+import { ingredientsSlice } from './slices/IngredientsSlice';
+import { constructorSlice } from './slices/constructorIngredientSlice';
+import { userSlice } from './slices/userSlice';
+import { feedsSlice } from './slices/feedsSlice';
+import { newOrderSlice } from './slices/newOrderSlice';
+import { userOrdersSlice } from './slices/userOrdersSlice';
 
-describe('Срез конструктора', () => {
-  beforeEach(() => {
-    store.dispatch(clearAll());
+describe('проверка инициализации rootReducer', () => {
+  const newStore = configureStore({
+    reducer: {
+      [ingredientsSlice.name]: ingredientsSlice.reducer,
+      [constructorSlice.name]: constructorSlice.reducer,
+      [userSlice.name]: userSlice.reducer,
+      [feedsSlice.name]: feedsSlice.reducer,
+      [newOrderSlice.name]: newOrderSlice.reducer,
+      [userOrdersSlice.name]: userOrdersSlice.reducer
+    }
   });
 
-  it('должен иметь корректное начальное состояние', () => {
-    const state = store.getState().constructorIngredient;
-
-    expect(state).toEqual({
+  it('проверка инициализации конструктора', () => {
+    expect(newStore.getState()[constructorSlice.name]).toEqual({
       bun: null,
       ingredients: []
     });
   });
 
-  it('должен позволять добавлять ингредиент', () => {
-    const mockIngredient = {
-      _id: '1',
-      name: 'Ингредиент 1',
-      type: 'sauce',
-      proteins: 10,
-      fat: 5,
-      carbohydrates: 20,
-      calories: 250,
-      price: 100,
-      image: '',
-      image_large: '',
-      image_mobile: ''
-    };
-
-    store.dispatch(addItem(mockIngredient));
-    const newState = store.getState().constructorIngredient;
-
-    expect(newState.ingredients).toHaveLength(1);
-    expect(newState.ingredients).toContainEqual(
-      expect.objectContaining({ _id: mockIngredient._id })
-    );
-  });
-
-  it('должен позволять очищать состояние', () => {
-    const mockIngredient = {
-      _id: '2',
-      name: 'Ингредиент 2',
-      type: 'sauce',
-      proteins: 56,
-      fat: 5,
-      carbohydrates: 20,
-      calories: 250,
-      price: 100,
-      image: '',
-      image_large: '',
-      image_mobile: ''
-    };
-
-    store.dispatch(addItem(mockIngredient));
-    expect(store.getState().constructorIngredient.ingredients).toHaveLength(1); // Убедитесь, что ингредиент добавлен
-
-    store.dispatch(clearAll());
-    expect(store.getState().constructorIngredient).toEqual({
-      bun: null,
-      ingredients: []
+  it('проверка инициализации ингредиентов', () => {
+    expect(newStore.getState()[ingredientsSlice.name]).toEqual({
+      error: null,
+      ingredients: [],
+      loading: false
     });
   });
 
-  it('должен позволять обновлять все ингредиенты', () => {
-    const mockIngredients = [
-      {
-        _id: '3',
-        name: 'Ингредиент 3',
-        type: 'sauce',
-        proteins: 10,
-        fat: 5,
-        carbohydrates: 20,
-        calories: 250,
-        price: 100,
-        image: '',
-        image_large: '',
-        image_mobile: ''
+  it('проверка инициализации ленты заказов', () => {
+    expect(newStore.getState()[feedsSlice.name]).toEqual({
+      orders: [],
+      total: 0,
+      totalToday: 0,
+      error: undefined,
+      isLoading: true
+    });
+  });
+
+  it('проверка инициализации пользователей', () => {
+    expect(newStore.getState()[userSlice.name]).toEqual({
+      user: {
+        email: '',
+        name: ''
       },
-      {
-        _id: '4',
-        name: 'Ингредиент 4',
-        type: 'sauce',
-        proteins: 20,
-        fat: 10,
-        carbohydrates: 30,
-        calories: 300,
-        price: 150,
-        image: '',
-        image_large: '',
-        image_mobile: ''
-      }
-    ];
-
-    store.dispatch(updateAll(mockIngredients));
-    expect(store.getState().constructorIngredient.ingredients).toHaveLength(2);
-    expect(store.getState().constructorIngredient.ingredients).toEqual(
-      mockIngredients
-    );
+      isAuthChecked: false,
+      error: ''
+    });
   });
+});
+
+it('проверка начального состояния хранилища', () => {
+  const expected = store.getState();
+  expect(expected).toEqual(store.getState());
 });
