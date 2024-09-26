4775e9a7e25e6caa9053ec19ffc9b915af95a353
Gaponenko Vadim <mandarinsev@yandex.ru>
2024-09-26 13:51:44 +0300

    исправлены замечания по проекту

--- src/pages/feed/feed.tsx
+++ src/pages/feed/feed.tsx
@@ -8,26 +8,21 @@ import { useDispatch, useSelector } from '../../services/store';
 export const Feed: FC = () => {
   /** TODO: взять переменную из стора */
   const dispatch = useDispatch();
+  const orders: TOrder[] = useSelector(getOrdersFeeds);
 
   useEffect(() => {
-    dispatch(getAllFeeds());
-  }, []);
-
-  const orders: TOrder[] = useSelector(getOrdersFeeds);
+    dispatch(getAllFeeds()), [];
+  });
 
   if (!orders.length) {
     return <Preloader />;
   }
 
-  const handleGetAllFeeds = () => {
-    dispatch(getAllFeeds());
-  };
-
   return (
     <FeedUI
       orders={orders}
       handleGetFeeds={() => {
-        handleGetAllFeeds;
+        dispatch(getAllFeeds());
       }}
     />
   );
